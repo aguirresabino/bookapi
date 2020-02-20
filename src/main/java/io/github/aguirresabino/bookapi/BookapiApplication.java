a94413679aba9cfafe5198f1ef375fc079b1ee3b
Aguirre Sabino <aguirresabino187@gmail.com>
2020-02-20 20:29:18 -0300

    Adiciona plugin sonar-maven-plugin em pom.xml

--- src/main/java/io/github/aguirresabino/bookapi/BookapiApplication.java
+++ src/main/java/io/github/aguirresabino/bookapi/BookapiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookapiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BookapiApplication.class, args);
+  }
 }
--- src/test/java/io/github/aguirresabino/bookapi/BookapiApplicationTests.java
+++ src/test/java/io/github/aguirresabino/bookapi/BookapiApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
