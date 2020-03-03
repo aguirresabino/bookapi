4f2ae5f9d36a26631dfd0c6efab1f8b099845328
Aguirre Sabino <aguirresabino187@gmail.com>
2020-03-03 17:43:09 -0300

    Ativa auditoria em BookRepository

--- src/main/java/io/github/aguirresabino/bookapi/repositories/BookRepository.java
+++ src/main/java/io/github/aguirresabino/bookapi/repositories/BookRepository.java
@@ -1,10 +1,12 @@
 package io.github.aguirresabino.bookapi.repositories;
 
 import io.github.aguirresabino.bookapi.models.Book;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@JaversSpringDataAuditable
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByIsbn(String isbn);
