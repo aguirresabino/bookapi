6b7ba3bea60fbaaffeb59ac343bfb233f7e1b4d5
Aguirre Sabino <aguirresabino187@gmail.com>
2020-03-04 10:23:54 -0300

    Adiciona novos atributos na entidade Book

--- src/main/java/io/github/aguirresabino/bookapi/models/Book.java
+++ src/main/java/io/github/aguirresabino/bookapi/models/Book.java
@@ -1,28 +1,51 @@
 package io.github.aguirresabino.bookapi.models;
 
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @EqualsAndHashCode
 @ToString
 public class Book {
 
   @Id @GeneratedValue private Long id;
 
+  @NotBlank(message = "Title is mandatory")
+  private String title;
+
+  private String subtitle;
+
+  @NotNull(message = "Publication date is mandatory")
+  private ZonedDateTime published;
+
+  private String publisher;
+
+  @NotNull(message = "Number of pages is mandatory")
+  private Integer pageCount;
+
+  @ElementCollection
+  @CollectionTable(name = "authors")
+  @NotEmpty
+  private List<String> author;
+
+  @URL private String thumbnailUrl;
+
+  @Column(columnDefinition = "TEXT")
+  private String shortDescription;
+
   @Column(unique = true)
   @NotBlank
   private String isbn;
-
-  @NotBlank private String title;
-  @NotBlank private String author;
 }
